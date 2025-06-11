ae5d75ccd934574d6c26fd246245954ec1d6a00a
David Olaleye <olaoluwaolaleye@gmail.com>
2025-06-11 10:47:46 -0500

    chore: add ads-management and ads-management/create route

--- src/pages/dashboard/adsManagement/index.tsx
+++ src/pages/dashboard/adsManagement/index.tsx
@@ -1,13 +1,22 @@
 import { ReactComponent as CheckIcon } from "../../../assets/create-add.svg";
+import { Link } from "react-router-dom";
 import styles from "./createAdd.module.css";
 
 export default function CreateAdd() {
   return (
     <>
-      <div>Create Add</div>
-      <div className={styles.createAdd}>
-        <CheckIcon />
-        <button>Create Add</button>
+      <div className={styles.headerFlex}>
+        <div>
+          <h1>ADS Management</h1>
+          <p>Here is a view of your messages</p>
+        </div>
+
+        <Link to="/ads-management/create" className={styles.createAddLink}>
+          <div className={styles.createAdd}>
+            <CheckIcon />
+            <button>Create Add</button>
+          </div>
+        </Link>
       </div>
     </>
   );
